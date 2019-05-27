0d93c0579c718814f4f33ff77c4333a4a85f1f5a
ZoeyZolotova <zoey.zolotova@gmail.com>
2019-05-27 17:48:46 +1000

    Remove fake requirements from spoiler log logic.

--- Models/ItemLogic.cs
+++ Models/ItemLogic.cs
@@ -31,6 +31,24 @@ namespace MMRando.Models
             RequiredItemIds = itemObject.DependsOnItems?.ToList();
             ConditionalItemIds = itemObject.Conditionals?.Select(c => c.ToList()).ToList();
             IsFakeItem = ItemUtils.IsFakeItem(ItemId);
+
+            // Remove fake requirements
+            switch (ItemId)
+            {
+                case Items.UpgradeBigBombBag:
+                case Items.MaskBlast:
+                    RequiredItemIds = null;
+                    break;
+                case Items.BottleCatchPrincess:
+                case Items.BottleCatchBigPoe:
+                    RequiredItemIds.Remove(Items.BottleCatchEgg);
+                    RequiredItemIds.Remove(Items.BottleCatchBug);
+                    RequiredItemIds.Remove(Items.BottleCatchFish);
+                    break;
+                case Items.BottleCatchEgg:
+                    RequiredItemIds.Remove(Items.BottleCatchFish);
+                    break;
+            }
         }
     }
 }
