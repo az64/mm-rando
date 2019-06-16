fb6d1a8424f51c1db69767283427043d76dcf33a
ZoeyZolotova <zoey.zolotova@gmail.com>
2019-06-16 23:15:06 +1000

    Allow starting with Gilded Sword and Mirror Shield since they can't be downgraded anymore.

--- Randomizer.cs
+++ Randomizer.cs
@@ -52,8 +52,6 @@ namespace MMRando
         // Starting items should not be replaced by trade items, or items that can be downgraded.
         private readonly ReadOnlyCollection<int> ForbiddenStartingItems = new List<int>
             {
-                Items.UpgradeGildedSword,
-                Items.UpgradeMirrorShield,
                 Items.UpgradeBiggestQuiver,
                 Items.UpgradeBigBombBag,
                 Items.UpgradeBiggestBombBag,
